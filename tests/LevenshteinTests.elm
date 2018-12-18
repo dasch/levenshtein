79ef30b7804babb11a173ee34718fab8dc3b6f35
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2018-12-18 13:09:06 +0100

    Use fuzz testing

--- tests/LevenshteinTests.elm
+++ tests/LevenshteinTests.elm
@@ -12,12 +12,12 @@ suite =
         [ fuzz Fuzz.string "distance is zero for identical strings" <|
             \example ->
                 Expect.equal 0 (distance example example)
-        , test "distance is the length of the first string if the second string is empty" <|
-            \_ ->
-                Expect.equal 5 (distance "hello" "")
-        , test "distance is the length of the second string if the first string is empty" <|
-            \_ ->
-                Expect.equal 5 (distance "" "hello")
+        , fuzz Fuzz.string "distance is the length of the first string if the second string is empty" <|
+            \example ->
+                Expect.equal (String.length example) (distance example "")
+        , fuzz Fuzz.string "distance is the length of the second string if the first string is empty" <|
+            \example ->
+                Expect.equal (String.length example) (distance "" example)
         , test "distance is the Levenshtein distance" <|
             \_ ->
                 Expect.equal 3 (distance "kitten" "sitting")
