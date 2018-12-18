5a17c331ec07140af9ebe7a9e6af8d19eecd4d15
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2018-12-18 13:04:23 +0100

    Refactor a bit

--- src/Levenshtein.elm
+++ src/Levenshtein.elm
@@ -9,27 +9,34 @@ distance str1 str2 =
 
         len2 =
             String.length str2
-
-        indicator =
-            if chr1 /= chr2 then
-                1
-
-            else
-                0
-
-        chr1 =
-            String.right 1 str1
-
-        chr2 =
-            String.right 1 str2
     in
     if min len1 len2 == 0 then
         max len1 len2
 
     else
+        let
+            chr1 =
+                String.right 1 str1
+
+            chr2 =
+                String.right 1 str2
+
+            indicator =
+                if chr1 /= chr2 then
+                    1
+
+                else
+                    0
+
+            newStr1 =
+                String.dropRight 1 str1
+
+            newStr2 =
+                String.dropRight 1 str2
+        in
         min
             (min
-                (distance (String.dropRight 1 str1) str2 + 1)
-                (distance str1 (String.dropRight 1 str2) + 1)
+                (distance newStr1 str2 + 1)
+                (distance str1 newStr2 + 1)
             )
-            (distance (String.dropRight 1 str1) (String.dropRight 1 str2) + indicator)
+            (distance newStr1 newStr2 + indicator)
